581d1d8fa33578687e8d21bbe6b927044b99eb5b
sundyli <543950155@qq.com>
2021-05-30 10:09:35 +0800

    remove unused codes

--- clickhouse-rs-cityhash-sys/target/debug/deps/cc-4f3d0916f8d2cc5f.d
+++
@@ -1,8 +0,0 @@
-/Users/sundy/pan/rust_proj/clickhouse-rs/clickhouse-rs-cityhash-sys/target/debug/deps/cc-4f3d0916f8d2cc5f.rmeta: /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/lib.rs /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/windows_registry.rs
-
-/Users/sundy/pan/rust_proj/clickhouse-rs/clickhouse-rs-cityhash-sys/target/debug/deps/libcc-4f3d0916f8d2cc5f.rlib: /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/lib.rs /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/windows_registry.rs
-
-/Users/sundy/pan/rust_proj/clickhouse-rs/clickhouse-rs-cityhash-sys/target/debug/deps/cc-4f3d0916f8d2cc5f.d: /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/lib.rs /Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/windows_registry.rs
-
-/Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/lib.rs:
-/Users/sundy/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.67/src/windows_registry.rs:
--- clickhouse-rs-cityhash-sys/target/debug/deps/clickhouse_rs_cityhash_sys-7d864db476db35a1.d
+++
@@ -1,5 +0,0 @@
-/Users/sundy/pan/rust_proj/clickhouse-rs/clickhouse-rs-cityhash-sys/target/debug/deps/clickhouse_rs_cityhash_sys-7d864db476db35a1.rmeta: src/lib.rs
-
-/Users/sundy/pan/rust_proj/clickhouse-rs/clickhouse-rs-cityhash-sys/target/debug/deps/clickhouse_rs_cityhash_sys-7d864db476db35a1.d: src/lib.rs
-
-src/lib.rs:
